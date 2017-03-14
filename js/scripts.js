6d4ad5253c730269b6bafff0079613a6670bf0e9
Łukasz Wasiluk <galawanty@gmail.com>
2017-03-14 17:55:42 +0100

    poprawki

--- js/scripts.js
+++ js/scripts.js
@@ -65,13 +65,7 @@ function newGame() {
 
 }
 
-function playerPick(playerPick) {
-    console.log(playerPick);
-}
 
-var x = Math.random();
-
-Math.floor(Math.random()*3)
 
 function getComputerPick() {
     var possiblePicks = ['rock', 'paper', 'scissors'];
